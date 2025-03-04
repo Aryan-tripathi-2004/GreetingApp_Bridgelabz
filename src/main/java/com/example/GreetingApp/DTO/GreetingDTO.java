b71a4c7a515f4dab7beb7266d9f8b88fc8c8bad5
Om Datt Tripathi <aryantripathi1606@gmail.com>
2025-03-04 16:42:39 +0530

    [Om Datt Tripathi] Using
    GreetingController
    
    return JSON for different
    HTTP Methods.

---
+++ src/main/java/com/example/GreetingApp/DTO/GreetingDTO.java
@@ -0,0 +1,23 @@
+package com.example.GreetingApp.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class GreetingDTO {
+    private String message;
+
+    public GreetingDTO(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
