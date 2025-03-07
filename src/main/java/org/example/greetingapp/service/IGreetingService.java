f236ba6810665e676a1ac9c58f6403e91fc47aed
Om Datt Tripathi <aryantripathi1606@gmail.com>
2025-03-07 11:52:41 +0530

    [Om Datt Tripathi] Add. Ability for the Greeting
    App to List all the
    
    Greeting Messages in
    
    the Repository

--- src/main/java/org/example/greetingapp/service/IGreetingService.java
+++ src/main/java/org/example/greetingapp/service/IGreetingService.java
@@ -2,9 +2,11 @@ package org.example.greetingapp.service;
 
 import org.example.greetingapp.dto.GreetingDTO;
 import org.example.greetingapp.dto.UserDTO;
+import java.util.List;
 
 
 public interface IGreetingService {
     GreetingDTO addGreeting(UserDTO user);
     GreetingDTO getGreetingById(long id);
+    List<GreetingDTO> getAllGreetings();
 }
